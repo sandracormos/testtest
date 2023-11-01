131a0e94d58478c90469c5f7fbb6975ab0aaac0a
DESKTOP-OGNF42M\FairyMental <sandracormos2002@gmail.com>
2023-11-01 22:02:05 +0200

    needle is alive

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -7,20 +7,22 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.test.R;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
     TextView height;
     TextView weight;
-
-    String weightStatus;
     TextView status;
-
     ImageView chart;
     ImageView needle;
 
-    @SuppressLint("MissingInflatedId")
+    TextView AXA;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,68 +33,103 @@ public class MainActivity extends AppCompatActivity {
         weight = findViewById(R.id.weight);
         final TextView resultText = findViewById(R.id.result);
         status = findViewById(R.id.status);
-        chart = findViewById(R.id.chart); // Make sure the ID matches your layout
+        chart = findViewById(R.id.chart);
         needle = findViewById(R.id.needle);
+        AXA =   findViewById(R.id.AXA);
+
+        // Define the minimum and maximum X-axis positions for the needle
+        final int minPosition = 0;
+        final int maxPosition = chart.getWidth() - needle.getWidth();
+        float myNeedlePos = needle.getX();
+
+        AXA.setText(Float.toString(myNeedlePos) );
+
+        AXA.setText(Float.toString(chart.getWidth()));
+
+
 
         btn.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint({"DefaultLocale", "SetTextI18n"})
+            @SuppressLint("DefaultLocale")
             @Override
             public void onClick(View v) {
-                // Get the user's input for height and weight
                 String heightStr = height.getText().toString();
                 String weightStr = weight.getText().toString();
 
-                // Check if the input fields are not empty
                 if (heightStr.isEmpty() || weightStr.isEmpty()) {
                     resultText.setTextSize(20);
                     resultText.setText("Please enter both height and weight");
                     return;
                 }
 
-                // Convert the input strings to numeric values
                 double heightValue = Double.parseDouble(heightStr);
                 double weightValue = Double.parseDouble(weightStr);
 
-                // Calculate the BMI
-                double heightInMeters = heightValue / 100.0; // Convert height from cm to meters
+                double heightInMeters = heightValue / 100.0;
                 double bmi = weightValue / (heightInMeters * heightInMeters);
 
+                String weightStatus;
                 if (bmi < 18.5) {
                     weightStatus = "Underweight";
-                } else if (bmi < 24.9 && bmi > 18.5) {
+                } else if (bmi < 24.9) {
                     weightStatus = "Normal weight";
-                } else if (bmi < 29.9 && bmi > 25) {
+                } else if (bmi < 29.9) {
                     weightStatus = "Overweight";
-                } else if (bmi >= 30) {
+                } else {
                     weightStatus = "Obesity";
                 }
 
-                // Display the calculated BMI
+
                 resultText.setText(String.format("Your BMI: %.2f", bmi));
                 status.setText(String.format("( %s )", weightStatus));
 
-                // Calculate the rotation angle for the needle based on BMI and update the needle's rotation
-                float rotationAngle = calculateRotationAngle(bmi);
-                needle.setRotation(rotationAngle);
+                // Calculate the X-axis position for the needle based on BMI
+                float position = calculatePosition(bmi, maxPosition);
+
+                // Set the X-axis position of the needle
+
             }
         });
     }
 
-    // Calculate the rotation angle based on the BMI value
-    private float calculateRotationAngle(double bmi) {
-        // Define your logic here to map BMI values to rotation angles
-        // Adjust this logic based on your chart design.
-        // This is just a placeholder logic; replace it with your own.
+    // Calculate the X-axis position based on the BMI value and chart design
+    private float calculatePosition(double bmi, int maxPosition) {
+        // Define the chart's minimum and maximum BMI values
 
-        float minBmi = 10.0f;
-        float maxBmi = 30.0f;
-        float minAngle = -45.0f; // Corresponds to BMI 10
-        float maxAngle = 45.0f;  // Corresponds to BMI 30
+        if(bmi < 10)
+            bmi = 10;
+        else if(bmi > 30)
+            bmi = 30;
 
-        float angleRange = maxAngle - minAngle;
-        float bmiRange = maxBmi - minBmi;
+        float normalizedBmi = (float) (bmi - 10) / (30 - 10);
+        float position = normalizedBmi * 1000 - 500;
 
-        // Calculate the rotation angle
-        return minAngle + (float) ((bmi - minBmi) / bmiRange * angleRange);
+        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) needle.getLayoutParams();
+        params.horizontalBias = normalizedBmi; // here is one modification for example. modify anything else you want :)
+        needle.setLayoutParams(params); // request the view to use the new modified params
+
+        return position;
+
+//        double minBmi = 10.0;
+//        double maxBmi = 30.0;
+//        float position = 0;
+//
+//        if (bmi > 30) {
+//            bmi = 30;
+//        } else if (bmi < 10) {
+//            bmi = 10;
+//        }
+//        // Calculate the X-axis position based on the user's BMI
+//        if (bmi < 20) {
+//            float aux = (float) ((bmi - minBmi) / 10) * 500;  //  450
+//            position = (float) -500+ aux;
+//        }
+//        if (bmi > 20) {
+//            float aux = (float) ((bmi - minBmi) / 10)*500;  //  9/10
+//            position = aux - 500;
+//
+//
+//        }
     }
+
+
 }
